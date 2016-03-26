1051db3d34f155be01115863630267e7209783ca
grumpyfogey <gerald.rapior@web.de>
2016-03-26 19:55:48 +0100

    v1.0 Final version of number puzzle

---
+++ Permutation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberPuzzle
+{
+    /// <summary>
+    /// Taken from:https://gist.github.com/fdeitelhoff/5052484
+    /// </summary>
+    static class Permutation
+    {
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IList<T> v)
+        {
+            ICollection<IList<T>> result = new List<IList<T>>();
+
+            Permute(v, v.Count, result);
+
+            return result;
+        }
+
+        private static void Permute<T>(IList<T> v, int n, ICollection<IList<T>> result)
+        {
+            if (n == 1)
+            {
+                result.Add(new List<T>(v));
+            }
+            else
+            {
+                for (var i = 0; i < n; i++)
+                {
+                    Permute(v, n - 1, result);
+                    Swap(v, n % 2 == 1 ? 0 : i, n - 1);
+                }
+            }
+        }
+
+        private static void Swap<T>(IList<T> v, int i, int j)
+        {
+            var t = v[i];
+            v[i] = v[j];
+            v[j] = t;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,50 @@ using System.Text;
 
 namespace NumberPuzzle
 {
+    /// <summary>
+    /// <para>A little number puzzle.</para>
+    /// <para>Arrange the numbers 1 to 9 in such a way that:</para>
+    /// <para>a * bc = de   (bc means 10*b+c)</para>
+    /// <para>       + fg</para>
+    /// <para>       = hi</para>
+    /// <para>The code for heap's algorithm can be found here: https://gist.github.com/fdeitelhoff/5052484 </para>
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var permutationsNumbers = numbers.Permute();
+
+            int numberOfPermutation = 1;
+
+            foreach (var permutation in permutationsNumbers)
+            {
+                List<int> listPermutation = permutation.ToList();
+
+                int firstInt = listPermutation[0] * (10*listPermutation[1] + listPermutation[2]);
+                int secondInt = 10 * listPermutation[3] + listPermutation[4];
+                int thirdInt = 10 * listPermutation[5] + listPermutation[6];
+                int fourthInt = 10 * listPermutation[7] + listPermutation[8];
+
+                Console.Write("{0}\r", numberOfPermutation);
+
+                if (firstInt == secondInt)
+                {
+                    if (secondInt + thirdInt == fourthInt)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6} {7} {8}", listPermutation[0],
+                            listPermutation[1], listPermutation[2], listPermutation[3],
+                            listPermutation[4], listPermutation[5], listPermutation[6],
+                            listPermutation[7], listPermutation[8]);
+                    }
+                }
+
+                numberOfPermutation++;
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
